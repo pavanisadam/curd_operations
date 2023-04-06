fe4db38944b0dad0a5df69d1a0a3235c06d5d8b9
pavanisadam <pavanisadam77@gmail.com>
2023-04-06 22:21:34 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -5,8 +5,15 @@ def display_topics(request):
     LOT=Topic.objects.all()
     d={'topics':LOT}
     return render(request,'display_topics.html',context=d)
-    
+
 def display_webpages(request):
     LOW=Webpage.objects.all()
     d={'webpages':LOW}
     return render(request,'display_webpages.html',context=d)
+
+def display_access(request):
+    LOA=AccessRecord.objects.all()
+    d={'access':LOA}
+    return render(request,'display_access.html',d)
+
+
